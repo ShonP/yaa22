52f50d8003a661ace2933fb2eeaf3437858065c6
Shon Pazarker <pazshon1@gmail.com>
2017-12-14 10:38:10 +0200

    first commit

---
+++ Scripts/main.js
@@ -0,0 +1,14 @@
+﻿
+function GetIE() {
+    var ua = window.navigator.userAgent;
+    var mise = ua.indexOf("MSIE ");
+    if (mise > 0)
+        return (parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+    return -1;
+}
+
+
+$(document).ready(function () {
+    $("#heading").fadeIn('slow');
+
+});
